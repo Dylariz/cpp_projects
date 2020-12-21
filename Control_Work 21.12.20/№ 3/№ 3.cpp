a898fa6016dec5ab7b052a1f726ffbeaf6ee7fa5
Dylariz <zarec-k@mail.ru>
2020-12-21 14:51:34 +0300

    Add №3

---
+++ Control_Work 21.12.20/№ 3/№ 3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* arr = new int[n]; // создаём указатель на массив
+
+    for (size_t i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int nMin;
+    for (int i = 0; i < n - 1; i++)
+    {
+        nMin = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[nMin])
+                nMin = j;
+        if (i != nMin)
+        {
+            swap(arr[i], arr[nMin]);
+        }
+    }
+
+    if (arr[0] >= 100)
+        cout << 0;
+    else 
+    {
+        for (size_t i = 0; i < n; i++)
+        {
+            if (arr[i] < 100)
+                cout << arr[i] << " ";
+            else
+                break;
+        }
+    }
+
+    delete[] arr; // отчистка массива
+    return 0;
+}
